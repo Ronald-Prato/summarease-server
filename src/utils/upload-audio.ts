e5454c13d8335be69a57021b2b70762810ad03d9
Ronald Prato <ronald.prato@unet.edu.ve>
2023-05-23 22:03:19 -0500

    sockets implemented

--- src/utils/upload-audio.ts
+++ src/utils/upload-audio.ts
@@ -1,10 +1,15 @@
 import multer from "multer";
 
+const getAbsolutePath = () => {
+  const prevDirname = __dirname.split("/");
+  return prevDirname.slice(0, prevDirname.length - 2).join("/");
+};
+
 export const storage = multer.diskStorage({
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   },
   destination: function (req, file, cb) {
-    cb(null, "audios");
+    cb(null, `${__dirname}/../../audios`);
   },
 });
