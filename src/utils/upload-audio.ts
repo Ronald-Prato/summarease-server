671047b81dad3a10a954dea28b34386f2bf09405
Ronald Prato <ronald.prato@unet.edu.ve>
2023-05-25 02:31:33 -0500

    change of absolute path for files saving

--- src/utils/upload-audio.ts
+++ src/utils/upload-audio.ts
@@ -1,15 +1,10 @@
 import multer from "multer";
 
-const getAbsolutePath = () => {
-  const prevDirname = __dirname.split("/");
-  return prevDirname.slice(0, prevDirname.length - 2).join("/");
-};
-
 export const storage = multer.diskStorage({
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   },
   destination: function (req, file, cb) {
-    cb(null, `${__dirname}/../../audios`);
+    cb(null, `${__dirname}/audios`);
   },
 });
